86253062565d42de7f806034d95bdb3897f34814
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-10-01 12:14:33 -0300

    Check native credentials only if necessary

--- app/sagas/selectServer.js
+++ app/sagas/selectServer.js
@@ -74,19 +74,18 @@ const handleSelectServer = function* handleSelectServer({ server, version, fetch
 				};
 				user = { ...user, roles: JSON.parse(user.roles) };
 			} catch (e) {
-				// do nothing?
+				// if not have user on db we check on native credentials
+				const servers = yield RNUserDefaults.objectForKey(SERVERS);
+				const userCredentials = servers && servers.find(srv => srv[SERVER_URL] === server);
+				user = userCredentials && {
+					token: userCredentials[TOKEN].length > userCredentials[USER_ID].length ? userCredentials[TOKEN] : userCredentials[USER_ID]
+				};
 			}
 		}
 
-		const servers = yield RNUserDefaults.objectForKey(SERVERS);
-		const userCredentials = servers && servers.find(srv => srv[SERVER_URL] === server);
-		const userLogin = userCredentials && {
-			token: userCredentials[TOKEN].length > userCredentials[USER_ID].length ? userCredentials[TOKEN] : userCredentials[USER_ID]
-		};
-
-		if (user || userLogin) {
-			yield RocketChat.connect({ server, user: user || userLogin });
-			yield put(setUser(user || userLogin));
+		if (user) {
+			yield RocketChat.connect({ server, user });
+			yield put(setUser(user));
 			yield put(actions.appStart('inside'));
 		} else {
 			yield RocketChat.connect({ server });
