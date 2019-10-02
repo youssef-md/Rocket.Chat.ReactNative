39646adb4a2f9e399730e260a432fe2ee98f0d65
diegolmello <diegolmello@gmail.com>
2019-10-02 11:48:30 -0300

    Don't use red colors on notification

--- android/app/src/main/java/chat/rocket/reactnative/CustomPushNotification.java
+++ android/app/src/main/java/chat/rocket/reactnative/CustomPushNotification.java
@@ -49,10 +49,6 @@ public class CustomPushNotification extends PushNotification {
             .setDefaults(Notification.DEFAULT_ALL)
             .setAutoCancel(true);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            notification.setColor(mContext.getColor(R.color.notification_text));
-        }
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
                                                                   CHANNEL_NAME,
